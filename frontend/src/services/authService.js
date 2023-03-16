b65377c43d345befc2fd53f83fdddb509f7a4315
Pedro Lucas <100699063+Pedro-L-Lopes@users.noreply.github.com>
2023-03-16 12:27:29 -0300

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -5,13 +5,12 @@ const register = async(data) => {
     const config = requestConfig("POST", data)
 
     try {
-        // Esperar uma resposta de um await fetch chamando a url de api + url que quero acessar para a função e depois mando o config
         const res = await fetch(api + "/users/register", config)
-            .then((res) => res.json()) // Then onde recebo dados e transforma em json
-            .catch((err) => err) // Se der algo de errado cai no catch e retorna o error
+            .then((res) => res.json())
+            .catch((err) => err)
 
-            if(res){                        // JSON.stringify(res) === transforma em string novamente a resposta
-                localStorage.setItem("user", JSON.stringify(res)) // Salvando na localStorage para ver depois se o uso está logado ou não
+            if(res){
+                localStorage.setItem("user", JSON.stringify(res))
             }
             return res
     } catch (error) {
@@ -29,10 +28,9 @@ const login = async (data) => {
     const config = requestConfig("POST", data)
 
     try {
-        // Esperando resposta da requisição fecth para a url api + "users/login"
         const res = await fetch(api + "/users/login", config)
-            .then((res) => res.json()) // Transforma o texto que chega na req em objeto 
-            .catch((err) => err) // Retorna possiveis erros
+            .then((res) => res.json())
+            .catch((err) => err)
 
         if(res){
             localStorage.setItem("user", JSON.stringify(res))
