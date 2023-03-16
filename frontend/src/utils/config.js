3a12b0036813ccde2b10888f58f9a82b326fe7dc
Pedro Lucas <100699063+Pedro-L-Lopes@users.noreply.github.com>
2023-03-16 12:22:13 -0300

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,27 +1,26 @@
 export const api = "http://localhost:5000/api"
 export const uploads = "http://localhost:5000/uploads"
 
-// Metodo da requisição // Dados que serão enviados // Token começa como null prevendo que o usuario não está logado
 export const requestConfig = (method, data, token = null, image = null) => {
 
     let config 
 
-    if(image){ // Requisição de imagem // Formato formData 
+    if(image){  
         config = {
             method,
             body: data, 
-            headers: {} // Precisa do headers mas pode deixar vazio
+            headers: {} 
         }
-    } else if(method === "DELETE" || data === null){ // Quando não precisa de dados ex: like
+    } else if(method === "DELETE" || data === null){ 
         config = {
             method,
-            headers: {} // Precisa do headers mas pode deixar vazio
+            headers: {} 
         }
-    } else { // Quando vem dados // Json
+    } else { 
         config = {
             method,
             body: JSON.stringify(data),
-            headers: { // headers obrigatorio
+            headers: { 
                 "Content-Type" : "application/json"
             }
         }
