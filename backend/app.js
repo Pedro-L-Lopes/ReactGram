187b853a4e221373d8c106fe1777fbfc447c1564
Pedro Lucas <100699063+Pedro-L-Lopes@users.noreply.github.com>
2023-03-16 12:15:40 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,22 @@
-require("dotenv").config() // Da acesso ao arquivo .env
+require("dotenv").config() 
 
 const express = require("express")
-const path = require("path") // Do propio nodeJs para determinar depois onde será o diretorio das imagens 
-const cors = require("cors") // Acessar o projeto pelo front-end
+const path = require("path") 
+const cors = require("cors") 
 
-const port = process.env.PORT // Acessando pelo arquivo .env
+const port = process.env.PORT 
 
-const app = express() // Invocando o framework
+const app = express() 
 
 // config JSON e form data response
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 // Solve CORS
-app.use(cors({credentials: true, origin: "http://localhost:3000"})) //Local onde está hospedado para a requisição funcionar
-
+app.use(cors({credentials: true, origin: "http://localhost:3000"})) /
+    
 //Upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
-//express.static() === Esta pasta vai conter arquivos estaticos
-//path.join(__dirname, "/uploads") === Juntando o nome do diretorio atual com "/uploads"
 
 // DB connection
 require("./config/db.js")
@@ -28,7 +26,6 @@ const router = require("./routes/Router.js")
 
 app.use(router)
 
-// Iniciando aplicação
 app.listen(port, () => {
-    console.log(`App rodando na porta ${port}`)
+    console.log(`App running in port ${port}`)
 })
