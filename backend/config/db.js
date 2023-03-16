cce206b3fe8fb73a46f724758797068d8ae477ff
Pedro Lucas <100699063+Pedro-L-Lopes@users.noreply.github.com>
2023-03-16 12:01:51 -0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,14 +6,14 @@ const conn = async () => {
     try {
         const dbConn = await mongoose.connect(
             `mongodb+srv://${dbUser}:${dbPassWord}@cluster0.zeqelhi.mongodb.net/?retryWrites=true&w=majority`
-        ) //Tentando conectar no banco 
+        ) 
 
-        console.log("Conectou ao banco!")
+        console.log("Connected")
 
         return dbConn
     } catch (error) {
         console.log(error)
-        console.log("Deu ruim!")
+        console.log("Failed to connect")
     }
 }
 
