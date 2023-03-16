31ad6831292117eef14bae7df4cb0d19edd7c751
Pedro Lucas <100699063+Pedro-L-Lopes@users.noreply.github.com>
2023-03-16 12:07:05 -0300

    Update handleValidation.js

--- backend/middlewares/handleValidation.js
+++ backend/middlewares/handleValidation.js
@@ -1,21 +1,17 @@
 const {validationResult} = require("express-validator")
 
-// Como é um middleware ele recebe a requisição a resposta e o next que é quando quer ou não prosseguir baseado em algum fato
 const validate = (req, res, next) => { 
 
-    const errors = validationResult(req) // Toda req que tiver o midleware de validação vai retornar possiveis erros
-
-    if(errors.isEmpty()) { // Verificando se os erros estão vazios
-        return next() // Se não tiver erros prossegue
+    const errors = validationResult(req) /
+    if(errors.isEmpty()) { 
+        return next() 
     }
 
-    // Erros extraidos da requisição
     const extractedErros = []
 
-    // Transfroma em array e faz um map
-    errors.array().map((err) => extractedErros.push(err.msg)) //pegando cada um e colocando a msg na extractedErros
 
-    // Returnando erros
+    errors.array().map((err) => extractedErros.push(err.msg)) 
+
     return res.status(422).json({
         errors: extractedErros
     })
